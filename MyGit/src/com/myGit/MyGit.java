2b1f76632a46c288fd9dedacded03bd286689ae7
MrYang <MrYang@126.com>
2017-08-17 20:37:15 +0800

    update1001

--- MyGit/src/com/myGit/MyGit.java
+++ MyGit/src/com/myGit/MyGit.java
@@ -4,5 +4,9 @@ public class MyGit {
 	public static void main(String[] args) {
 		System.out.println("first");
 		System.out.println("second");
+		System.out.println("tttt");
+		System.out.println("tttt");
+		System.out.println("tttt");
+		System.out.println("tttt");
 	}
 }
