1fadef715ba67798477815804dec53f8afa76793
MarkChburg <1766163080@qq.com>
2017-08-17 20:16:43 +0800

    new sysout third

--- MyGit/src/com/myGit/NewSys.java
+++ MyGit/src/com/myGit/NewSys.java
@@ -5,6 +5,7 @@ public class NewSys {
 	public static void main(String[] args) {
 		System.out.println("new sysout first");
 		System.out.println("new sysout second");
+		System.out.println("new sysout third");
 	}
 
 }
